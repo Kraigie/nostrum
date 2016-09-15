f6c97ec41f78cd417dd9ed07118b3c8206bca1af
Craig Dazey <craigdazey@gmail.com>
2016-09-15 11:40:05 -0400

    add role struct

---
+++ lib/mixcord/constructs/role.ex
@@ -0,0 +1,29 @@
+defmodule Mixcord.Constructs.Role do
+  @moduledoc """
+  Struct representing a Discord role and various helper functions.
+  """
+
+  @doc """
+  Defines the Role struct.
+
+  * :id -	*String*. Id of the role.
+  * :name - *String*. Name of the role.
+  * :color - *Integer*. Integer representation of hexadecimal color code.
+  * :hoist - *Boolean*. If the role is pinned in the user listing.
+  * :position - *Integer*. Position of the role.
+  * :permissions - *Integer*. Permission bit set.
+  * :managed - *Boolean*. Whether this role is managed by an integration.
+  * :mentionable - *Boolean*. Whether this role is mentionable.
+  """
+  @derive [Poison.Encoder]
+  defstruct [
+    :id
+    :name
+    :color
+    :hoist
+    :position
+    :permissions
+    :managed
+    :mentionable
+  ]
+end
