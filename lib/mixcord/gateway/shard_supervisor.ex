885ee07034e13c857a434168da016c0fe4df1fb8
Craig Dazey <craigdazey@gmail.com>
2016-09-20 13:16:56 -0400

    changed supervisor for future methods

--- lib/mixcord/gateway/shard_supervisor.ex
+++ lib/mixcord/gateway/shard_supervisor.ex
@@ -2,14 +2,21 @@ defmodule Mixcord.ShardSupervisor do
   use Supervisor
 
   def start_link(token, num_shards // 1) do
-    Supervisor.start_link(__MODULE__, %{token: token, num_shards: num_shards})
+    Supervisor.start_link(__MODULE__, [token: token, num_shards: num_shards])
   end
 
   def init(options) do
-    children = for i <- 1..options.num_shards do
-      worker(Mixbot.Shard, [%{handler: __MODULE__, shard_num: i, token: options.token}])
-     end
+    children = for i <- 1..options[:num_shards], do: create_worker(i, options[:token])
 
      supervise(children, strategy: :one_for_one)
+  end
+
+  def create_worker(shard_num, token) do
+    #TODO: Add shard struct to map here with PID
+    worker(Mixbot.Shard, [%{handler: __MODULE__, shard_num: i, token: options[:token]}])
+  end
+
+  def update_status(status) do
+    #for shard in shard cache, shard.updateStatus(cache.pid, status)
   end
 end
