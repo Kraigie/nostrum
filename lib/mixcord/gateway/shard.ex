b9fafba46b6123223bce27f5dec1d08521368e13
Craig Dazey <craigdazey@gmail.com>
2016-09-22 10:29:15 -0400

    seperate shard helpers from ws impl for readability

--- lib/mixcord/gateway/shard.ex
+++ lib/mixcord/gateway/shard.ex
@@ -1,14 +1,10 @@
 defmodule Mixcord.Shard do
-  @moduledoc """
-  """
+  @moduledoc false
 
   alias Mixcord.Constants
-  alias Mixcord.Rest.Client
+  import Mixcord.Shard.Helpers
 
   @behaviour :websocket_client
-
-  #TODO: Struct?
-  #TODO: Docs
 
   #hide sharding
   def start_link(token, caller) do
@@ -23,76 +19,41 @@ defmodule Mixcord.Shard do
     :websocket_client.start_link(gateway() , __MODULE__, state_map)
   end
 
-  def status_update(pid, new_status) do
-    send(pid, {:status_update, new_status})
-  end
+  def websocket_handle({:binary, payload}, _state, state_map) do
+    payload = :erlang.binary_to_term(payload)
 
-  @doc false
-  def websocket_handle(any, _state, state_map) do
-    case any do
-      _ -> IO.inspect(any) #state_map.caller.do_something(msg) to run users commands
+    case Constants.name_from_opcode payload.op do
+      "EVENT" ->
+        handle_event()
+      "HELLO" ->
+        identify()
+      #state_map.caller.do_something(msg) to run users commands
     end
 
     {:ok, state_map}
   end
 
-  @doc false
-  def send_heartbeat(time) do
-    Process.send_after(self, :heartbeat, time)
-  end
-
-  @doc false
   def init(state_map) do
     {:once, state_map}
   end
 
-  @doc false
   def onconnect(_ws_req, state_map) do
     #identify :websocket_client.cast(self, {:text, "message"})
-    IO.inspect("CONNECTED")
     {:ok, state_map}
   end
 
-  @doc false
   def ondisconnect(reason, state) do
     {:close, reason, state}
   end
 
-  @doc false
   def websocket_info({:status_update, new_status}, _ws_req, state) do
     #TODO: Flesh this out
     :websocket_client.cast(self, {new_status})
     {:ok, state}
   end
 
-  @doc false
   def websocket_terminate(_close_info, _ws_req, _state) do
     :ok
-  end
-
-  @doc false
-  def gateway do
-    if Enum.member?(:ets.all, :gateway_url) do
-      url = :ets.lookup(:gateway_url, "url")
-        |> List.first
-      url["url"]
-    else
-      :ets.new(:gateway_url, [:named_table])
-      new_url = get_new_gateway_url()
-      :ets.insert(:gateway_url, {"url", new_url})
-      new_url
-    end
-  end
-
-  defp get_new_gateway_url do
-    case Client.request(:get, Constants.gateway, "") do
-      {:error, status_code: status_code, message: message} ->
-        raise(Mixcord.Errors.ApiError, status_code: status_code, message: message)
-      {:ok, body: body} ->
-        body = Poison.decode!(body)
-        gateway_url = body["url"] <> "?encoding=etf&v=6"
-          |> to_charlist
-    end
   end
 
 end
---
+++ lib/mixcord/gateway/shard_helpers.ex
@@ -0,0 +1,52 @@
+defmodule Mixcord.Shard.Helpers do
+  @moduledoc """
+  """
+
+  alias Mixcord.Constants
+  alias Mixcord.Rest.Client
+
+  def status_update(pid, new_status) do
+    send(pid, {:status_update, new_status})
+  end
+
+  def handle_event do
+
+  end
+
+  def identify do
+    
+  end
+
+  @doc false
+  def send_heartbeat(time) do
+    Process.send_after(self, :heartbeat, time)
+  end
+
+  @doc false
+  def gateway do
+    if Enum.member?(:ets.all, :gateway_url) do
+      IO.inspect "FOUND OLD GATEWAY"
+      url = :ets.lookup(:gateway_url, "url")
+        |> List.first
+      url["url"]
+    else
+      IO.inspect "GETTING NEW GATEWAY"
+      :ets.new(:gateway_url, [:named_table])
+      new_url = get_new_gateway_url()
+      :ets.insert(:gateway_url, {"url", new_url})
+      new_url
+    end
+  end
+
+  defp get_new_gateway_url do
+    case Client.request(:get, Constants.gateway, "") do
+      {:error, status_code: status_code, message: message} ->
+        raise(Mixcord.Errors.ApiError, status_code: status_code, message: message)
+      {:ok, body: body} ->
+        body = Poison.decode!(body)
+        gateway_url = body["url"] <> "?encoding=etf&v=6"
+          |> to_charlist
+    end
+  end
+
+end
