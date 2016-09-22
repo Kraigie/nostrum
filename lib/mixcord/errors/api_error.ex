2e3fb1505ee1141a2900ff9b8131974d20cea929
Craig Dazey <craigdazey@gmail.com>
2016-09-22 11:49:01 -0400

    error messages are sometimes atoms, cast them to strings

--- lib/mixcord/errors/api_error.ex
+++ lib/mixcord/errors/api_error.ex
@@ -1,9 +1,14 @@
 defmodule Mixcord.Errors.ApiError do
   defexception [:message]
 
-  def exception(message: message, status_code: status_code) do
-    msg = "Status Code: #{status_code}: " <> message
+  def exception(status_code: status_code, message: message) when is_atom(message) do
+    msg = "ERROR: #{status_code} #{to_string(message)}"
     %__MODULE__{message: msg}
   end
-  
+
+  def exception(status_code: status_code, message: message) when is_binary(message) do
+    msg = "ERROR: #{status_code} #{to_string(message)}"
+    %__MODULE__{message: msg}
+  end
+
 end
