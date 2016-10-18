a8b50bca6e19ca2677335bf7aec052e5a2cd01ec
Craig Dazey <craigdazey@gmail.com>
2016-10-18 13:22:58 -0400

    change var state_map to state
    I still remember when I didn't know how elixir worked and I assumed the state variable was some voodoo magic shit but I was actually just retarded

--- lib/mixcord/shard/shard.ex
+++ lib/mixcord/shard/shard.ex
@@ -18,86 +18,86 @@ defmodule Mixcord.Shard do
     :websocket_client.start_link(gateway, __MODULE__, state_map(token, caller, shard_num))
   end
 
-  def websocket_handle({:binary, payload}, _state, state_map) do
+  def websocket_handle({:binary, payload}, _state, state) do
     payload = :erlang.binary_to_term(payload)
     new_state =
       case Constants.name_from_opcode payload.op do
         "DISPATCH" ->
           #TODO: Task.start this?
-          handle_dispatch(payload, state_map)
-          state_map =
+          handle_dispatch(payload, state)
+          state =
             if payload.t == :READY do
-              %{state_map | session: payload.d.session_id}
+              %{state | session: payload.d.session_id}
             else
-              state_map
+              state
             end
 
-          %{state_map | reconnect_attempts: 0}
+          %{state | reconnect_attempts: 0}
         "HEARTBEAT" ->
           Logger.debug "GOT HEARTBEAT PING"
-          :websocket_client.cast(self, {:binary, heartbeat_payload(state_map.seq)})
-          state_map
+          :websocket_client.cast(self, {:binary, heartbeat_payload(state.seq)})
+          state
         "HEARTBEAT_ACK" ->
-          heartbeat_intervals = state_map.heartbeat_intervals
+          heartbeat_intervals = state.heartbeat_intervals
             |> List.delete_at(-1)
-            |> List.insert_at(0, state_map.last_heartbeat - now())
-          %{state_map | heartbeat_intervals: heartbeat_intervals}
+            |> List.insert_at(0, state.last_heartbeat - now())
+          %{state | heartbeat_intervals: heartbeat_intervals}
         "HELLO" ->
           #TODO: Check for resume if session is set, build resume packet and send - will also need to restart heartbeat
           heartbeat(self, payload.d.heartbeat_interval)
           identify(self)
-          state_map
+          state
         "INVALID_SESSION" ->
           Logger.debug "INVALID_SESSION"
           identify(self)
-          state_map
+          state
         "RECONNECT" ->
           Logger.debug "RECONNECT"
-          state_map
+          state
       end
     {:ok, %{new_state | seq: payload.s}}
   end
 
-  def websocket_info({:status_update, new_status_json}, _ws_req, state_map) do
+  def websocket_info({:status_update, new_status_json}, _ws_req, state) do
     #TODO: Flesh this out - Idle time?
     :websocket_client.cast(self, {:binary, status_update_payload(new_status_json)})
-    {:ok, state_map}
+    {:ok, state}
   end
 
-  def websocket_info({:heartbeat, interval}, _ws_req, state_map) do
+  def websocket_info({:heartbeat, interval}, _ws_req, state) do
     now = now()
-    :websocket_client.cast(self, {:binary, heartbeat_payload(state_map.seq)})
+    :websocket_client.cast(self, {:binary, heartbeat_payload(state.seq)})
     heartbeat(self, interval)
-    {:ok, %{state_map | last_heartbeat: now}}
+    {:ok, %{state | last_heartbeat: now}}
   end
 
-  def websocket_info(:identify, _ws_req, state_map) do
-    :websocket_client.cast(self, {:binary, identity_payload(state_map)})
-    {:ok, state_map}
+  def websocket_info(:identify, _ws_req, state) do
+    :websocket_client.cast(self, {:binary, identity_payload(state)})
+    {:ok, state}
   end
 
-  def init(state_map) do
-    {:once, state_map}
+  def init(state) do
+    {:once, state}
   end
 
-  def onconnect(_ws_req, state_map) do
-    Logger.debug "SHARD #{state_map.shard_num} CONNECTED"
-    {:ok, state_map}
+  def onconnect(_ws_req, state) do
+    Logger.debug "SHARD #{state.shard_num} CONNECTED"
+    {:ok, state}
   end
 
-  def ondisconnect(reason, state_map) do
+  def ondisconnect(reason, state) do
     Logger.debug "WS DISCONNECTED BECAUSE: #{inspect reason}"
-    Logger.debug "STATE ON CLOSE: #{inspect state_map}"
-    if state_map.reconnect_attempts > 3 do
-      {:close, reason, state_map}
+    Logger.debug "STATE ON CLOSE: #{inspect state}"
+    if state.reconnect_attempts > 3 do
+      {:close, reason, state}
     else
       :timer.sleep(5000)
-      Logger.debug "RECONNECT ATTEMPT NUMBER #{state_map.reconnect_attempts + 1}"
-      {:reconnect, %{state_map | reconnect_attempts: state_map.reconnect_attempts + 1}}
+      Logger.debug "RECONNECT ATTEMPT NUMBER #{state.reconnect_attempts + 1}"
+      {:reconnect, %{state | reconnect_attempts: state.reconnect_attempts + 1}}
     end
   end
 
-  def websocket_terminate(reason, _ws_req, _state_map) do
+  def websocket_terminate(reason, _ws_req, _state) do
     #SO TRIGGERED I CANT GET END EVENT CODES
     Logger.debug "WS TERMINATED BECAUSE: #{inspect reason}"
     :ok
