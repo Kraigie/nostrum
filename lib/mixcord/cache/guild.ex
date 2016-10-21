0f3fd466560f03dfc4f0e677e5a156fb7b81dd5a
Craig Dazey <craigdazey@gmail.com>
2016-10-21 11:21:22 -0400

    switch caches to use ets
    ets allows for concurrent reads, writes are serialized by a GenServer

--- lib/mixcord/cache/guild.ex
+++ lib/mixcord/cache/guild.ex
@@ -5,33 +5,27 @@ defmodule Mixcord.Cache.Guild do
   use GenServer
 
   # TODO: Length\Update?
-  # TODO: https://www.djm.org.uk/posts/elixir-keyword-lists-function-parameters/
 
   def start_link do
     GenServer.start_link(__MODULE__, [], name: Guilds)
   end
 
-  def all() do
-    GenServer.call(Guilds, :all)
+  def init(_args) do
+    :ets.new(:guilds, [:set, :public, :named_table])
+    {:ok, []}
   end
 
-  def get(id: id), do: GenServer.call(Guilds, {:get, id: id})
-  def get(channel_id: channel_id), do: GenServer.call(Guilds, {:get, channel: channel_id})
+  def get(id: id), do: :ets.lookup_element(:guilds, id, 2)
+  def get(message: message), do: get(id: message.channel.guild_id)
 
   def get!(id: id) do
     get(id: id)
       |> bangify_find
   end
 
-  def get!(channel_id: channel_id) do
-    get(channel_id: channel_id)
+  def get!(message: message) do
+    get(message: message)
       |> bangify_find
-  end
-
-  def search(fun), do: GenServer.call(Guilds, {:search, fun})
-  def search!(fun) do
-    GenServer.call(Guilds, {:search, fun})
-    |> bangify_find
   end
 
   @doc false
@@ -44,31 +38,17 @@ defmodule Mixcord.Cache.Guild do
     GenServer.cast(Guilds, {:remove, id: id})
   end
 
-  def handle_call(:all, _from, state) do
-    {:reply, state, state}
+  defp handle_cast({:create, guild}, state) do
+    :ets.insert(:guilds, {guild.id, guild})
+    {:noreply, state}
   end
 
-  def handle_call({:get, id: id}, _from, state) do
-    {:reply, Enum.find(state, fn guild -> guild.id == id end), state}
+  defp handle_cast({:remove, id}, state) do
+    :ets.delete(:guilds, {id})
+    {:noreply, state}
   end
 
-  def handle_call({:get, channel_id: channel_id}, _from, state) do
-    {:reply, Enum.find(state, fn guild -> guild.id == channel_id end), state}
-  end
-
-  def handle_call({:search, fun}, _from, state) do
-    {:reply, Enum.find(state, fun), state}
-  end
-
-  def handle_cast({:create, guild}, state) do
-    {:noreply, state ++ guild}
-  end
-
-  def handle_cast({:remove, id}, state) do
-    {:noreply, Enum.filter(state, fn guild -> guild.id == id end)}
-  end
-
-  def bangify_find(to_bang) do
+  defp bangify_find(to_bang) do
     case to_bang do
       nil ->
         raise(Mixcord.Error.CacheError)
