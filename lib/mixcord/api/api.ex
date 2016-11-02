734517f69ce4c7529423d4780cd33b236ea8c683
Craig Dazey <craigdazey@gmail.com>
2016-11-02 14:00:25 -0400

    bangify api methods, add more docs

--- lib/mixcord/api/api.ex
+++ lib/mixcord/api/api.ex
@@ -106,40 +106,90 @@ defmodule Mixcord.Api do
     request(:get, Constants.channel(channel_id))
   end
 
+  def get_channel!(channel_id) do
+    get_channel(channel_id)
+    |> bangify
+  end
+
   def edit_channel(channel_id, options) do
     request(:patch, Constants.channel(channel_id), options)
+  end
+
+  def edit_channel!(channel_id, options) do
+    edit_channel(channel_id, options)
+    |> bangify
   end
 
   def delete_channel(channel_id) do
     request(:delete, Constants.channel(channel_id))
   end
 
+  def delete_channel!(channel_id) do
+    delete_channel(channel_id)
+    |> bangify
+  end
+
   def get_channel_messages(channel_id, options) do
     request(:get, Constants.channel_messages(channel_id), options)
+  end
+
+  def get_channel_messages!(channel_id, options) do
+    get_channel_messages(channel_id, options)
+    |> bangify
   end
 
   def get_channel_message(channel_id, message_id) do
     request(:get, Constants.channel_message(channel_id, message_id))
   end
 
+  def get_channel_message!(channel_id, message_id) do
+    get_channel_message(channel_id, message_id)
+    |> bangify
+  end
+
   def bulk_delete_messages(channel_id) do
     request(:delete, Constants.channel_bulk_delete(channel_id))
+  end
+
+  def bulk_delete_messages!(channel_id) do
+    bulk_delete_messages(channel_id)
+    |> bangify
   end
 
   def edit_channel_permissions(channel_id, overwrite_id) do
     request(:put, Constants.channel_permission(channel_id, overwrite_id))
   end
 
+  def edit_channel_permissions!(channel_id, overwrite_id) do
+    edit_channel_permissions(channel_id, overwrite_id)
+    |> bangify
+  end
+
   def delete_channel_permissions(channel_id, overwrite_id) do
     request(:delete, Constants.channel_permission(channel_id, overwrite_id))
+  end
+
+  def delete_channel_permissions!(channel_id, overwrite_id) do
+    delete_channel_permissions(channel_id, overwrite_id)
+    |> bangify
   end
 
   def get_channel_invites(channel_id) do
     request(:get, Constants.channel_invites(channel_id))
   end
 
+  def get_channel_invites!(channel_id) do
+    get_channel_invites(channel_id)
+    |> bangify
+  end
+
   def create_channel_invite(channel_id, options \\ %{}) do
     request(:post, Constants.channel_invites(channel_id))
+  end
+
+  def create_channel_invite!(channel_id, options \\ %{}) do
+    create_channel_invite(channel_id, options)
+    |> bangify
   end
 
   @doc """
@@ -154,6 +204,21 @@ defmodule Mixcord.Api do
     request(:post, Constants.channel_typing(channel_id))
   end
 
+
+  @doc """
+  Triggers the typing indicator.
+
+  Triggers the typing indicator in the channel specified by `channel_id`.
+  The typing indicator lasts for about 8 seconds and then automatically stops.
+
+  Raises `Mixcord.Error.ApiError` if error occurs while making the rest call.
+  Returns {:ok} if successful.
+  """
+  def start_typing!(channel_id) do
+    start_typing(channel_id)
+    |> bangify
+  end
+
   @doc """
   Pins a message.
 
@@ -166,6 +231,19 @@ defmodule Mixcord.Api do
   end
 
   @doc """
+  Pins a message.
+
+  Pins the message specified by `message_id` in the channel specified by `channel_id`.
+
+  Raises `Mixcord.Error.ApiError` if error occurs while making the rest call.
+  Returns {:ok} if successful.
+  """
+  def add_pinned_message!(channel_id, message_id) do
+    add_pinned_message(channel_id, message_id)
+    |> bangify
+  end
+
+  @doc """
   Unpins a message.
 
   Unpins the message specified by `message_id` in the channel specified by `channel_id`.
@@ -174,6 +252,19 @@ defmodule Mixcord.Api do
   """
   def delete_pinned_message(channel_id, message_id) do
     request(:delete, Constants.channel_pin(channel_id, message_id))
+  end
+
+  @doc """
+  Unpins a message.
+
+  Unpins the message specified by `message_id` in the channel specified by `channel_id`.
+
+  Raises `Mixcord.Error.ApiError` if error occurs while making the rest call.
+  Returns {:ok} if successful.
+  """
+  def delete_pinned_message!(channel_id, message_id) do
+    delete_pinned_message(channel_id, message_id)
+    |> bangify
   end
 
   def bangify(to_bang) do
