f25f8ae476ff5d7eb3625ac0c79c767fa72a7116
Craig Dazey <craigdazey@gmail.com>
2017-03-10 00:38:33 -0500

    version 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Nostrum.Mixfile do
   def project do
     [
       app: :nostrum,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
@@ -55,6 +55,7 @@ defmodule Nostrum.Mixfile do
       {:poison, "~> 3.0"},
       {:ex_doc, "~> 0.14", only: :dev},
       {:credo, "~> 0.4", only: [:dev, :test]},
+      # {:websocket_client, "~> 1.2"}
       {:websocket_client, git: "https://github.com/Kraigie/websocket_client.git"},
       {:gen_stage, "~> 0.11"}
     ]
