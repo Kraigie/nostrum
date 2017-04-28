bb2d7bf526bc0c363b7ca8133067f6c5388e7faf
sn0wmobil <snowmobil92@gmail.com>
2017-04-28 18:49:33 +0200

    Update api.ex
    content needs to be String not Map

--- lib/nostrum/api.ex
+++ lib/nostrum/api.ex
@@ -146,7 +146,7 @@ defmodule Nostrum.Api do
   end
 
   # Embeds
-  def create_message(channel_id, [content: c, embed: e], tts) when is_map(c) do
+  def create_message(channel_id, [content: c, embed: e], tts) when do
     request(:post, Constants.channel_messages(channel_id), %{content: c, embed: e, tts: tts})
     |> handle(Message)
   end
