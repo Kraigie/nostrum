c337d1e500a0aa0195f5561d178e85c93c392b51
Craig Dazey <craigdazey@gmail.com>
2017-12-07 13:20:28 -0500

    update deps to elixir v1.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Nostrum.Mixfile do
   def project do
     [
       app: :nostrum,
-      version: "0.2.1",
+      version: "0.3.0",
       elixir: "~> 1.4",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
@@ -52,11 +52,11 @@ defmodule Nostrum.Mixfile do
 
   defp deps do
     [
-      {:httpoison, "== 0.11.1"},
+      {:httpoison, "~> 0.13"},
       {:poison, "~> 3.0"},
       {:ex_doc, "~> 0.14", only: :dev},
       {:credo, "~> 0.4", only: [:dev, :test]},
-      {:gun, "~> 1.0.0-pre.2"},
+      {:gun, git: "https://github.com/ninenines/gun"},
       {:gen_stage, "~> 0.11"}
     ]
   end
