f0e0c7b9a901b9bd987e0b91e1a1a28483619246
Craig Dazey <craigdazey@gmail.com>
2018-03-24 21:19:37 -0400

    fix TaskedConsumer example

--- examples/event_consumer_supervisor.ex
+++ examples/event_consumer_supervisor.ex
@@ -12,6 +12,10 @@ defmodule ExampleTaskedConsumer do
   use Nostrum.TaskedConsumer
   alias Nostrum.Api
 
+  def start_link do
+    TaskedConsumer.start_link(__MODULE__)
+  end
+
   def handle_event({:MESSAGE_CREATE, {msg}, _ws_state}) do
     case msg.content do
       "!sleep" ->
