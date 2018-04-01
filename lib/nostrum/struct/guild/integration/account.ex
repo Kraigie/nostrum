fc5fa0b8b701f7d909f35d12ea7f9d4a1895714d
Zac Nowicki <zachnowicki@gmail.com>
2018-04-01 19:53:04 -0400

    Add @moduledoc tags missing from some structs

--- lib/nostrum/struct/guild/integration/account.ex
+++ lib/nostrum/struct/guild/integration/account.ex
@@ -1,2 +1,5 @@
 defmodule Nostrum.Struct.Guild.Integration.Account do
+  @moduledoc """
+  Struct representing a Discord guild's integration account.
+  """
 end
--- lib/nostrum/struct/user/connection.ex
+++ lib/nostrum/struct/user/connection.ex
@@ -1,2 +1,5 @@
 defmodule Nostrum.Struct.User.Connection do
+  @moduledoc """
+  Struct representing a Discord User's connections
+  """
 end
