5002ec950384440282beb13ca3c87ae75c1298e5
Justin <jbtcao@gmail.com>
2018-04-12 12:59:08 -0500

    Updated message-related structs
    Added Activity struct for messages
    
    Added Application struct for messages
    
    Cleaned up Message struct

---
+++ lib/nostrum/struct/message/activity.ex
@@ -0,0 +1,34 @@
+defmodule Nostrum.Struct.Message.Activity do
+  @moduledoc """
+  Struct representing a Discord message activity.
+  """
+
+  alias Nostrum.Util
+
+  defstruct [
+    :type,
+    :party_id
+  ]
+
+  @typedoc """
+  [Type of message activity](https://discordapp.com/developers/docs/resources/channel#message-object-message-activity-types).
+  """
+  @type type :: integer
+
+  @typedoc """
+  The party id from a [rich presence event](https://discordapp.com/developers/docs/rich-presence/how-to).
+  """
+  @type party_id :: String.t() | nil
+
+  @type t :: %__MODULE__{
+          type: type,
+          party_id: party_id
+        }
+
+  @doc false
+  def to_struct(map) do
+    new = map |> Map.new(fn {k, v} -> {Util.maybe_to_atom(k), v} end)
+
+    struct(__MODULE__, new)
+  end
+end
---
+++ lib/nostrum/struct/message/application.ex
@@ -0,0 +1,49 @@
+defmodule Nostrum.Struct.Message.Application do
+  @moduledoc """
+  Struct representing a Discord message application.
+  """
+
+  alias Nostrum.Struct.Snowflake
+  alias Nostrum.Util
+
+  defstruct [
+    :id,
+    :cover_image,
+    :description,
+    :icon,
+    :name
+  ]
+
+  @typedoc "Id of the application"
+  @type id :: Snowflake.t()
+
+  @typedoc "Id of the embed's image asset"
+  @type cover_image :: String.t()
+
+  @typedoc "Application's description"
+  @type description :: String.t()
+
+  @typedoc "Id of the application's icon"
+  @type icon :: String.t()
+
+  @typedoc "Name of the application"
+  @type name :: String.t()
+
+  @type t :: %__MODULE__{
+          id: id,
+          cover_image: cover_image,
+          description: description,
+          icon: icon,
+          name: name
+        }
+
+  @doc false
+  def to_struct(map) do
+    new =
+      map
+      |> Map.new(fn {k, v} -> {Util.maybe_to_atom(k), v} end)
+      |> Map.update(:id, nil, &Util.cast(&1, Snowflake))
+
+    struct(__MODULE__, new)
+  end
+end
