2143b2c3f70afba13ba781aae1f1984a46a79fb9
Justin <jbtcao@gmail.com>
2018-06-19 22:15:14 -0500

    Added return all perms if admin test
    Also fixed a logical error, because the test caught it!

--- test/nostrum/struct/guild/member_test.exs
+++ test/nostrum/struct/guild/member_test.exs
@@ -1,7 +1,10 @@
 defmodule Nostrum.Struct.MemberTest do
   use ExUnit.Case, async: true
 
+  alias Nostrum.Struct.Guild
   alias Nostrum.Struct.Guild.Member
+  alias Nostrum.Struct.Guild.Role
+  alias Nostrum.Struct.Permission
   alias Nostrum.Struct.User
 
   doctest Member
@@ -11,6 +14,17 @@ defmodule Nostrum.Struct.MemberTest do
       member = %Member{user: %User{id: 150_061_853_001_777_154}}
 
       assert(Member.mention(member) === User.mention(member.user))
+    end
+  end
+
+  describe "guild_permissions/2" do
+    test "returns all perms if admin" do
+      member = %Member{roles: [10]}
+      guild = %Guild{roles: [%Role{id: 10, permissions: Permission.to_bitset(:administrator)}]}
+
+      result = Member.guild_permissions(member, guild)
+
+      assert(result === Permission.all())
     end
   end
 
