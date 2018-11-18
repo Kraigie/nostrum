76eb0977761767fe5714ecf7abc2b4697f7ef936
Kevin Tindall <kevinkjt2000@gmail.com>
2018-11-18 13:19:57 -0600

    Add dependency to :inets

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Nostrum.Mixfile do
 
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :inets],
       mod: {Nostrum.Application, []}
     ]
   end
