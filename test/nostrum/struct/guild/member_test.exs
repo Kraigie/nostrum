64327acbb4e5898a25df6c37fc001498ed2d58fb
Kevin Tindall <kevinkjt2000@gmail.com>
2018-11-23 19:19:50 -0600

    Test empty channel permissions overlap

--- test/nostrum/struct/guild/member_test.exs
+++ test/nostrum/struct/guild/member_test.exs
@@ -136,6 +136,37 @@ defmodule Nostrum.Struct.MemberTest do
 
       assert(result === Permission.from_bitset(0x00000040))
     end
+
+    test "returns empty list when there are no matching ids between channel overrides and member roles" do
+      member_id = 500
+      guild_id = 100
+      role_id = 200
+      channel_id = 300
+      everyone_role_id = 100
+
+      member = %Member{user: %User{id: member_id}, roles: [role_id]}
+
+      everyone_role = %Role{id: everyone_role_id, permissions: 0}
+      role = %Role{id: role_id, permissions: 0}
+
+      channel = %Channel{
+        id: channel_id,
+        permission_overwrites: [
+          %Overwrite{id: role_id, allow: 0, deny: 0},
+          %Overwrite{id: member_id, allow: 0, deny: 0}
+        ]
+      }
+
+      guild = %Guild{
+        id: guild_id,
+        channels: %{channel.id => channel},
+        roles: %{everyone_role.id => everyone_role, role.id => role}
+      }
+
+      result = Member.guild_channel_permissions(member, guild, channel_id)
+
+      assert(result == [])
+    end
   end
 
   describe "String.Chars" do
