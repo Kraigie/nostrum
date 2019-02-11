7b1d50c1ac8e2b4ac16ff4d48819a728f8924bb8
Craig Dazey <craigdazey@gmail.com>
2019-02-11 16:16:09 -0500

    update version to 0.3.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Nostrum.Mixfile do
   def project do
     [
       app: :nostrum,
-      version: "0.3.0",
+      version: "0.3.1",
       elixir: "~> 1.7",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
