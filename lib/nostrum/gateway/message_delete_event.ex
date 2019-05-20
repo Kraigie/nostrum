ef289300cbf59d89fe8ad3ae7d40c434e6fc0674
Justin <jbtcao@gmail.com>
2019-05-20 15:09:52 -0500

    Added typedocs to `MessageDeleteEvent`

--- lib/nostrum/gateway/message_delete_event.ex
+++ lib/nostrum/gateway/message_delete_event.ex
@@ -11,8 +11,17 @@ defmodule Nostrum.Gateway.MessageDeleteEvent do
     :guild_id
   ]
 
+  @typedoc "Id of the deleted message."
   @type id :: Snowflake.t()
+
+  @typedoc "Channel id of the deleted message."
   @type channel_id :: Snowflake.t()
+
+  @typedoc """
+  Guild id of the deleted message.
+
+  `nil` if a non-guild message was deleted.
+  """
   @type guild_id :: Snowflake.t() | nil
 
   @type t :: %__MODULE__{
