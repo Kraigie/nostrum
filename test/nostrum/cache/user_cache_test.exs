0d3f3c8f5077527915995a4304f5bb7d065e36bd
Johannes Christ <jc@jchri.st>
2019-05-30 18:10:05 +0200

    Add tests for the user cache.

---
+++ test/nostrum/cache/user_cache_test.exs
@@ -0,0 +1,96 @@
+defmodule Nostrum.Cache.UserCacheTest do
+  use ExUnit.Case, async: true
+
+  @cache_implementations [
+    Nostrum.Cache.UserCache.ETS
+  ]
+
+  for cache <- @cache_implementations do
+    defmodule :"#{cache}Test" do
+      alias Nostrum.Struct.User
+      use ExUnit.Case
+
+      # this is needed because otherwise we cannot access
+      # the cache in the tests
+      @cache cache
+      @test_user %{
+        id: 12345,
+        username: "test",
+        discriminator: "1234",
+        avatar: nil,
+        bot: true,
+        mfa_enabled: nil,
+        verified: nil
+      }
+      @test_user_two %{
+        id: 54321,
+        username: "test two",
+        discriminator: "4321",
+        avatar: nil,
+        bot: true,
+        mfa_enabled: nil,
+        verified: nil
+      }
+
+      setup do
+        @cache.setup()
+        :ok
+      end
+
+      describe "bulk_create/1" do
+        test "returns `:ok`" do
+          users = [@test_user, @test_user_two]
+          assert :ok = @cache.bulk_create(users)
+        end
+      end
+
+      describe "create/1" do
+        test "returns a struct of the created user" do
+          expected = User.to_struct(@test_user)
+
+          assert ^expected = @cache.create(@test_user)
+        end
+      end
+
+      describe "delete/1" do
+        test "returns `:noop` on uncached user" do
+          assert :noop = @cache.delete(10_258_109_258_109_258_125)
+        end
+
+        test "returns user struct on cached user" do
+          expected = User.to_struct(@test_user)
+          @cache.create(@test_user)
+          assert ^expected = @cache.delete(@test_user.id)
+        end
+      end
+
+      describe "get/1" do
+        test "returns error tuple on unknown user" do
+          assert {:error, _reason} = @cache.get(120_815_092_581_902_580)
+        end
+
+        test "returns cached user on known user" do
+          expected = User.to_struct(@test_user)
+          @cache.create(@test_user)
+          assert {:ok, ^expected} = @cache.get(@test_user.id)
+        end
+      end
+
+      describe "update/1" do
+        test "returns `:noop` on uncached user" do
+          payload = %{id: 8_284_967_893_178_597_859_421}
+          assert :noop = @cache.update(payload)
+        end
+
+        test "returns `{before, after}` on cached user" do
+          expected_before = User.to_struct(@test_user)
+          updated_test_user = Map.put(@test_user, :name, "updated test user")
+          expected_after = User.to_struct(updated_test_user)
+          @cache.create(@test_user)
+
+          assert {^expected_before, ^expected_after} = @cache.update(updated_test_user)
+        end
+      end
+    end
+  end
+end
