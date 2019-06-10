f5beee4be74c4f22bd3c2fd8ede15f2b01612c71
Justin <jbtcao@gmail.com>
2019-06-09 20:28:44 -0500

    Added typedocs to MessageDeleteBulk.

--- lib/nostrum/struct/event/message_delete_bulk.ex
+++ lib/nostrum/struct/event/message_delete_bulk.ex
@@ -1,13 +1,32 @@
 defmodule Nostrum.Struct.Event.MessageDeleteBulk do
+  @moduledoc """
+  Struct representing a Message Delete Bulk event
+  """
+
+  alias Nostrum.Snowflake
+
   defstruct [
     :channel_id,
     :guild_id,
     :ids
   ]
 
+  @typedoc "Channel id of the deleted message"
+  @type channel_id :: Snowflake.t()
+
+  @typedoc """
+  Guild id of the deleted message
+
+  `nil` if a non-guild message was deleted.
+  """
+  @type guild_id :: Snowflake.t() | nil
+
+  @typedoc "Ids of the deleted messages"
+  @type ids :: [Snowflake.t(), ...]
+
   @type t :: %__MODULE__{
-    channel_id: Snowflake.t(),
-    guild_id: Snowflake.t() | nil,
-    ids: [Snowflake.t(), ...]
-  }
+          channel_id: channel_id,
+          guild_id: guild_id,
+          ids: ids
+        }
 end
