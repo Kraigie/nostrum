7f027412f180e1b4e0f6401c73378ec0bf55972e
skwerlman <skwerlman@users.noreply.github.com>
2019-08-01 04:13:11 -0400

    define voiceregion type

--- lib/nostrum/struct/voice_region.ex
+++ lib/nostrum/struct/voice_region.ex
@@ -2,4 +2,13 @@ defmodule Nostrum.Struct.VoiceRegion do
   @moduledoc """
   Struct representing a Discord voice region.
   """
+
+  @type t :: %{
+    custom: boolean,
+    deprecated: boolean,
+    id: String.t,
+    name: String.t,
+    optimal: boolean,
+    vip: boolean
+  }
 end
