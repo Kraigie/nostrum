6ae086c5704ce780138b431bf452cf6aec91eee4
Kraigie <craigdazey@gmail.com>
2020-05-14 19:33:44 -0400

    update version to 0.4.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Nostrum.Mixfile do
   def project do
     [
       app: :nostrum,
-      version: "0.4.1",
+      version: "0.4.2",
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       build_embedded: Mix.env() == :prod,
