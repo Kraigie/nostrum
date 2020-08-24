e73fb1bb7593f8a259dc20c504d184512e6d5234
Awlexus <8594605+Awlexus@users.noreply.github.com>
2020-08-24 20:24:09 +0000

    Update configs (#176)
    * Use Config module added in elixir 1.9
    
    * Add a way to add secret configs
    
    Co-authored-by: Awlex <no thank you>

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,9 @@
-use Mix.Config
+import Config
 
 config :nostrum,
   token: "",
   num_shards: :auto
 
 config :logger, :console, metadata: [:shard]
+
+if File.exists?("config/secret.exs"), do: import_config("secret.exs")
