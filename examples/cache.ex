fde905c491d32cbdfdc0fb36f30ff46e1a845ec8
Andris0 <Andris211@gmail.com>
2020-08-27 07:29:27 +0300

    Trimming code comments a bit

--- examples/cache.ex
+++ examples/cache.ex
@@ -23,15 +23,14 @@ defmodule ExampleConsumer do
     Consumer.start_link(__MODULE__)
   end
 
-  # We only need to write specific event handlers for the events we are interested in,
-  # which in this case, will be the handling of new Discord messages,
-  # for other cases, there is a catch all handler at the end of the module.
+  # We only need to write event handlers for the events we are interested in,
+  # the rest will go to the catch-all case to be ignored.
   def handle_event({:MESSAGE_CREATE, message, _ws_state}) do
     ExampleCommands.command(message)
   end
 
-  # The catch all event case that takes and ignores all events you are not
-  # interested in, if you do not have this, or have not defined literally
+  # The catch-all event case that takes the rest of the events.
+  # If you do not have this, or have not defined literally
   # every event case yourself (what an absolute madlad if you have),
   # the consumer will crash, not having any function signature to match upon.
   # By the way, the return atom stands for "no-op", shorthand for "no operation",
@@ -61,12 +60,12 @@ defmodule ExampleCommands do
     end
   end
 
-  # The command that will be handling our message.
-  # Pattern matching's pretty neat by the way, a nice way to define a shape for the data we accept
-  # and have it reject the rest, also gets rid of a good chunk of all that pesky condition checking
-  # that you would normally see, trying for that data shape that we need, destructuring's also pretty neat.
-  # For this, a simple `userinfo` command that accepts a user id and returns a message
-  # containing some things you would need to fetch from the cache (or the API, as a backup).
+  # The command that will be handling our message. Use pattern matching to
+  # enforce the data shape that you accept, while also destructuring data for convenience.
+  # This also helps avoiding unnecessary conditional checks in the function body,
+  # that would usually introduce a need for early returns in other languages.
+  # For this, a simple `userinfo` command that accepts a user id and returns a message,
+  # containing information you would be fetching from the cache (or the API, as a backup).
   def command(%{
         content: @prefix <> "userinfo " <> message_user_id,
         channel_id: channel_id,
@@ -74,17 +73,16 @@ defmodule ExampleCommands do
       })
       when guild_id != nil do
     # Our happy path for sending a complete userinfo response.
-    # Pretty standard stuff, maybe apart from additionally wrapping both sides in a tuple
-    # with an extra identifier, that's just to make failure cases more obvious.
+    # We'll wrap both sides of the first one with an extra identifier,
+    # so we can more easily match on cases where parsing would fail.
     with {{user_id, _binary}, :parse_id} when is_snowflake(user_id) <-
            {Integer.parse(message_user_id), :parse_id},
-         {{:ok, user}, :user} <-
-           {get_cached_with_fallback(user_id, &UserCache.get/1, &Api.get_user/1), :user},
-         {{:ok, %{name: channel_name}}, :channel} <-
-           {get_cached_with_fallback(channel_id, &ChannelCache.get/1, &Api.get_channel/1),
-            :channel},
-         {{:ok, %{name: guild_name}}, :guild} <-
-           {get_cached_with_fallback(guild_id, &GuildCache.get/1, &Api.get_guild/1), :guild} do
+         {:ok, user} <-
+           get_cached_with_fallback(user_id, &UserCache.get/1, &Api.get_user/1),
+         {:ok, %{name: channel_name}} <-
+           get_cached_with_fallback(channel_id, &ChannelCache.get/1, &Api.get_channel/1),
+         {:ok, %{name: guild_name}} <-
+           get_cached_with_fallback(guild_id, &GuildCache.get/1, &Api.get_guild/1) do
       Api.create_message(
         channel_id,
         """
@@ -94,32 +92,20 @@ defmodule ExampleCommands do
         """
       )
     else
-      # Failure case for parsing and (somewhat) validating the User ID.
-      # Pretty easy to tell which point of failure this case handles,
-      # if we would match only on the return value of `Integer.parse/2`,
-      # the error value for that function is literally just `:error`,
-      # not very descriptive, is it... would not be easy to tell from a glance
-      # where in the happy path a failure occurred, if you do not know the error
-      # return values of the happy path functions in that very moment you were reading this.
-      # This kind of pattern gets pretty handy when return values are quite descriptionless or
-      # you have multiple functions that return the same thing, but also need to be handled differently.
-      # Either way, a pattern I do not see enough of, but would like to, where it's needed.
+      # Since we have multiple failure patterns from the combination of `Integer.parse/2`
+      # and `is_snowflake/1`, we'll use the identifier as the term to match on instead.
       {_invalid_id, :parse_id} ->
         Api.create_message(channel_id, "Make sure you entered a valid User ID")
 
-      # The cache + API failure case with the additional resource identifier, pretty straight forward,
-      # resource type for clarity, and then whatever the API throws at us as the reason, though the
-      # stringified API reason is for development purposes, not something you would throw at a regular
-      # user, but you know, this is just an example after all, make it all nice and pretty.
-      {{:error, reason}, resource} ->
-        Api.create_message(channel_id, "Unable to retrieve #{resource}: #{inspect(reason)}")
+      # The cache + API failure case. For now, lets just return the stringified failure
+      # reason of whatever the API returned. Up to you if you want to make it all nice and pretty.
+      {:error, reason} ->
+        Api.create_message(channel_id, "Failed to retrieve all required info: #{inspect(reason)}")
     end
   end
 
-  # Just like handling events, a fall through `command` case to catch and ignore unwanted cases,
-  # otherwise it will go boom, KABLAM, "OH GOD NO!", *insert mushroom cloud here*, "AAAAH, MY FACE,
-  # IT'S MELTING!", well not really, since we have supervision trees, fault tolerance and the absolute,
-  # massive powerhouse of awesomeness that the BEAM is and all that jazz, but you know, you catch my drift.
-  # Also, "noop", *giggles*.
+  # Just like handling events in the consumer,
+  # a catch-all case for the things we do not care about.
+  # The good 'ol "nooper" (yes, as in "noose", you can't stop me)
   def command(_invalid_command), do: :noop
 end
