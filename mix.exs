2bd7056eff18b149374439c25712ae08182fbbec
Kraigie <craigdazey@gmail.com>
2021-05-26 19:52:26 -0400

    bump version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Nostrum.Mixfile do
   def project do
     [
       app: :nostrum,
-      version: "0.4.6",
+      version: "0.4.7",
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       build_embedded: Mix.env() == :prod,
