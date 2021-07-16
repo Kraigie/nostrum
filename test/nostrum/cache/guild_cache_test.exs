a34cae320477f0db1688f921af46b618f9c33e51
Johannes Christ <jc@jchri.st>
2021-07-16 13:41:44 +0200

    Fix tests.

--- test/nostrum/cache/guild_cache_test.exs
+++ test/nostrum/cache/guild_cache_test.exs
@@ -2,14 +2,12 @@ defmodule Nostrum.Cache.GuildCacheTest do
   use ExUnit.Case
 
   alias Nostrum.Cache.CacheSupervisor
-  alias Nostrum.Cache.Guild.GuildRegister
   alias Nostrum.Cache.GuildCache
   alias Nostrum.Struct.Guild
 
   setup_all do
-    CacheSupervisor.start_link([])
-    GuildRegister.create_guild_process(0, %Guild{id: 0})
-
+    start_supervised!(CacheSupervisor, [])
+    assert true = GuildCache.create(%Guild{id: 0})
     :ok
   end
 
