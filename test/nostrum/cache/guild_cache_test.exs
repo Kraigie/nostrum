474de6093dc74747764bc2d1c20604ab60a8f640
Johannes Christ <jc@jchri.st>
2021-07-18 01:14:11 +0200

    Create ETS table in tests.

--- test/nostrum/cache/guild_cache_test.exs
+++ test/nostrum/cache/guild_cache_test.exs
@@ -1,12 +1,11 @@
 defmodule Nostrum.Cache.GuildCacheTest do
   use ExUnit.Case
 
-  alias Nostrum.Cache.CacheSupervisor
   alias Nostrum.Cache.GuildCache
   alias Nostrum.Struct.Guild
 
   setup_all do
-    start_supervised!(CacheSupervisor, [])
+    :ets.new(GuildCache.tabname(), [:set, :public, :named_table])
     assert true = GuildCache.create(%Guild{id: 0})
     :ok
   end
