ac3b149d44f46a6190f0245ce2566a46bf544392
Benjamin Danklin <hellobenjamindanklin@gmail.com>
2021-08-03 22:55:28 +1000

    typedoc for channel types

--- lib/nostrum/struct/channel.ex
+++ lib/nostrum/struct/channel.ex
@@ -105,6 +105,21 @@ defmodule Nostrum.Struct.Channel do
   @typedoc "When the last pinned message was pinned"
   @type last_pin_timestamp :: String.t() | nil
 
+  @typedoc """
+  TYPE                 	|ID 	|DESCRIPTION
+  -------------------------------------------------------------------------------
+  GUILD_TEXT           	|0  	|a text channel within a server
+  DM                   	|1  	|a direct message between users
+  GUILD_VOICE          	|2  	|a voice channel within a server
+  GROUP_DM             	|3  	|a direct message between multiple users
+  GUILD_CATEGORY       	|4  	|an organizational category that contains up to 50 channels
+  GUILD_NEWS           	|5  	|a channel that users can follow and crosspost into their own server
+  GUILD_STORE          	|6  	|a channel in which game developers can sell their game on Discord
+  GUILD_NEWS_THREAD    	|10 	|a temporary sub-channel within a GUILD_NEWS channel
+  GUILD_PUBLIC_THREAD  	|11 	|a temporary sub-channel within a GUILD_TEXT channel
+  GUILD_PRIVATE_THREAD 	|12 	|a temporary sub-channel within a GUILD_TEXT channel that is only viewable by those invited and those with the MANAGE_THREADS permission
+  GUILD_STAGE_VOICE    	|13 	|a voice channel for hosting events with an audience
+  """
   @type type :: 0..13
 
   @typedoc """
