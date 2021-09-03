9f84072af9fc27f42da9d4f37136bc02850de236
The Major <spartan364@gmail.com>
2021-09-03 16:26:48 -0700

    make interaction fields optional

--- lib/nostrum/struct/interaction.ex
+++ lib/nostrum/struct/interaction.ex
@@ -93,15 +93,15 @@ defmodule Nostrum.Struct.Interaction do
   def to_struct(map) do
     %__MODULE__{
       id: map.id,
-      application_id: map.application_id,
+      application_id: map[:application_id],
       type: map.type,
       data: Util.cast(map[:data], {:struct, ApplicationCommandInteractionData}),
       guild_id: map[:guild_id],
       channel_id: map[:channel_id],
       member: Util.cast(map[:member], {:struct, Member}),
       user: Util.cast(map[:user], {:struct, User}),
-      token: map.token,
-      version: map.version,
+      token: map[:token],
+      version: map[:version],
       message: Util.cast(map[:message], {:struct, Message})
     }
   end
