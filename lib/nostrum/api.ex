219903d2a34a86cfba39d65f41f239717e21486a
Joe Banks <joe@jb3.dev>
2022-01-10 15:38:25 +0000

    Use new helpers for API datetime conversion

--- lib/nostrum/api.ex
+++ lib/nostrum/api.ex
@@ -1778,15 +1778,9 @@ defmodule Nostrum.Api do
 
   def modify_guild_member(guild_id, user_id, %{} = options, reason)
       when is_snowflake(guild_id) and is_snowflake(user_id) do
-    # Handle native timeout timestamp serialisation
-    disable_ts = options[:communication_disabled_until]
-
     options =
-      if disable_ts != nil do
-        %{options | communication_disabled_until: DateTime.to_iso8601(disable_ts)}
-      else
-        options
-      end
+      options
+      |> maybe_convert_date_time(:communication_disabled_until)
 
     request(%{
       method: :patch,
