cf2735ad1fa5fb2efb321aeec383c183d525135a
The Major <spartan364@gmail.com>
2022-03-01 19:01:00 +0000

    fix `thread_update` typespec

--- lib/nostrum/consumer.ex
+++ lib/nostrum/consumer.ex
@@ -222,8 +222,12 @@ defmodule Nostrum.Consumer do
   Dispatched when a thread is created or when added to a private thread
   """
   @type thread_create :: {:THREAD_CREATE, Channel.t(), WSState.t()}
-  @type thread_delete :: {:THREAD_DELETE, Channel.t(), WSState.t()}
-  @type thread_update :: {:THREAD_UPDATE, Channel.t(), WSState.t()}
+
+  @typedoc """
+  Dispatched when a thread is deleted, if the thread was cached, contains the original thread, otherwise contains `:noop`
+  """
+  @type thread_delete :: {:THREAD_DELETE, Channel.t() | :noop, WSState.t()}
+  @type thread_update :: {:THREAD_UPDATE, {old_thread :: Channel.t() | nil, new_thread :: Channel.t()}, WSState.t()}
 
   @typedoc """
   Dispatched when gaining access to a channel
