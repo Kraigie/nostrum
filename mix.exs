e7e6505afdf8674588996ddae2e252d697973f5e
Johannes Christ <jc@jchri.st>
2022-03-10 22:19:38 +0100

    Release v0.5.1

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Nostrum.Mixfile do
   def project do
     [
       app: :nostrum,
-      version: "0.5.0",
+      version: "0.5.1",
       elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       build_embedded: Mix.env() == :prod,
