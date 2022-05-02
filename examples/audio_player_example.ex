e3be6c5e9a57108e13909b8e1b8497aa1b081b2e
Brandt Hill <brandtlarsonhill@gmail.com>
2022-05-02 16:50:32 -0500

    Update audio example to use application commands

--- examples/audio_player_example.ex
+++ examples/audio_player_example.ex
@@ -29,68 +29,70 @@ defmodule AudioPlayerConsumer do
   # Audio file will be fed directly to ffmpeg
   @nut_file_url "https://brandthill.com/files/nut.wav"
 
+  # Compile-time helper for defining Discord Application Command options
+  opt = fn type, name, desc, opts ->
+    %{type: type, name: name, description: desc}
+    |> Map.merge(Enum.into(opts, %{}))
+  end
+
+  @play_opts [
+    opt.(1, "song", "Play a song", []),
+    opt.(1, "nut", "Play a nut sound", []),
+    opt.(1, "file", "Play a file", options: [opt.(3, "url", "File URL to play", required: true)]),
+    opt.(1, "url", "Play a URL from a common service",
+      options: [opt.(3, "url", "URL to play", required: true)]
+    )
+  ]
+
+  @commands [
+    {"summon", "Summon bot to your voice channel", []},
+    {"leave", "Tell bot to leave your voice channel", []},
+    {"play", "Play a sound", @play_opts},
+    {"stop", "Stop the playing sound", []},
+    {"pause", "Pause the playing sound", []},
+    {"resume", "Resume the paused sound", []}
+  ]
+
   def start_link do
     Consumer.start_link(__MODULE__)
   end
 
-  def get_voice_channel_of_msg(msg) do
-    msg.guild_id
+  def get_voice_channel_of_interaction(%{guild_id: guild_id, user: %{id: user_id}} = _interaction) do
+    guild_id
     |> GuildCache.get!()
     |> Map.get(:voice_states)
-    |> Enum.find(%{}, fn v -> v.user_id == msg.author.id end)
+    |> Enum.find(%{}, fn v -> v.user_id == user_id end)
     |> Map.get(:channel_id)
   end
 
-  def do_not_ready_msg(msg) do
-    Api.create_message(msg.channel_id, "I need to be in a voice channel for that.")
+  # If you are running this example in an iex session where you manually call
+  # AudioPlayerSupervisor.start_link, you will have to call this function
+  # with your guild_id as the argument
+  def create_guild_commands(guild_id) do
+    Enum.each(@commands, fn {name, description, options} ->
+      Api.create_guild_application_command(guild_id, %{
+        name: name,
+        description: description,
+        options: options
+      })
+    end)
   end
 
-  def handle_event({:MESSAGE_CREATE, msg, _ws_state}) do
-    case msg.content do
-      # The bot will search through the guild cache's voice states to find
-      # the voice channel that the message author is in to join.
-      "!summon" ->
-        case get_voice_channel_of_msg(msg) do
-          nil ->
-            Api.create_message(msg.channel_id, "Must be in a voice channel to summon")
+  def handle_event({:READY, %{guilds: guilds} = _event, _ws_state}) do
+    guilds
+    |> Enum.map(fn guild -> guild.id end)
+    |> Enum.each(&create_guild_commands/1)
+  end
 
-          voice_channel_id ->
-            Voice.join_channel(msg.guild_id, voice_channel_id)
-        end
+  def handle_event({:INTERACTION_CREATE, interaction, _ws_state}) do
+    # Run the command, and check for a response message, or default to a checkmark emoji
+    message =
+      case do_command(interaction) do
+        {:msg, msg} -> msg
+        _ -> ":white_check_mark:"
+      end
 
-      "!leave" ->
-        Voice.leave_channel(msg.guild_id)
-
-      # Following play song/nut commands check if connected
-      # and will let the user know in case of failure.
-      "!play song" ->
-        if Voice.ready?(msg.guild_id) do
-          Voice.play(msg.guild_id, @soundcloud_url, :ytdl)
-        else
-          do_not_ready_msg(msg)
-        end
-
-      "!play nut" ->
-        if Voice.ready?(msg.guild_id) do
-          Voice.play(msg.guild_id, @nut_file_url, :url)
-        else
-          do_not_ready_msg(msg)
-        end
-
-      # Following commands don't check anything so they'll
-      # fail quietly if nothing is playing/paused or in channel.
-      "!pause" ->
-        Voice.pause(msg.guild_id)
-
-      "!resume" ->
-        Voice.resume(msg.guild_id)
-
-      "!stop" ->
-        Voice.stop(msg.guild_id)
-
-      _ ->
-        :noop
-    end
+    Api.create_interaction_response(interaction, %{type: 4, data: %{content: message}})
   end
 
   def handle_event({:VOICE_SPEAKING_UPDATE, payload, _ws_state}) do
@@ -101,5 +103,39 @@ defmodule AudioPlayerConsumer do
   # you don't have a method definition for each event type.
   def handle_event(_event) do
     :noop
+  end
+
+  def do_command(%{guild_id: guild_id, data: %{name: "summon"}} = interaction) do
+    case get_voice_channel_of_interaction(interaction) do
+      nil ->
+        {:msg, "You must be in a voice channel to summon me"}
+
+      voice_channel_id ->
+        Voice.join_channel(guild_id, voice_channel_id)
+    end
+  end
+
+  def do_command(%{guild_id: guild_id, data: %{name: "leave"}}) do
+    Voice.leave_channel(guild_id)
+    {:msg, "See you later :wave:"}
+  end
+
+  def do_command(%{guild_id: guild_id, data: %{name: "pause"}}), do: Voice.pause(guild_id)
+
+  def do_command(%{guild_id: guild_id, data: %{name: "resume"}}), do: Voice.resume(guild_id)
+
+  def do_command(%{guild_id: guild_id, data: %{name: "stop"}}), do: Voice.stop(guild_id)
+
+  def do_command(%{guild_id: guild_id, data: %{name: "play", options: options}}) do
+    if Voice.ready?(guild_id) do
+      case options do
+        [%{name: "song"}] -> Voice.play(guild_id, @soundcloud_url, :ytdl)
+        [%{name: "nut"}] -> Voice.play(guild_id, @nut_file_url, :url)
+        [%{name: "file", options: [%{value: url}]}] -> Voice.play(guild_id, url, :url)
+        [%{name: "url", options: [%{value: url}]}] -> Voice.play(guild_id, url, :ytdl)
+      end
+    else
+      {:msg, "I must be in a voice channel before playing audio"}
+    end
   end
 end
