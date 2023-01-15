6aa682df05f488a798710ac06507adba3244819f
Awlex <no thank you>
2023-01-15 22:28:17 +0100

    Remove enabled/1 and disabled/1 in favour of enabled/2; fix toggle/1

--- lib/nostrum/struct/component/button.ex
+++ lib/nostrum/struct/component/button.ex
@@ -105,31 +105,18 @@ defmodule Nostrum.Struct.Component.Button do
   """
   def toggle(%{type: 2, disabled: button_state} = button) do
     button
-    |> struct({:disabled, !button_state})
+    |> struct([{:disabled, !button_state}])
   end
 
   def toggle(_), do: buttons_only()
 
   @doc """
-  Disables the button.
+  Enables the button when `enabled` is true. Disables it otherwise.
   """
-
-  def disable(%{type: 2} = button) do
+  def enable(%{type: 2} = button, enabled) do
     button
-    |> update({:disabled, true})
+    |> update([{:disabled, !enabled}])
   end
-
-  def disable(_), do: buttons_only()
-
-  @doc """
-  Enables the button.
-  """
-  def enable(%{type: 2} = button) do
-    button
-    |> update({:disabled, false})
-  end
-
-  def enable(_), do: buttons_only()
 
   @doc """
   Changes the style of the button.
