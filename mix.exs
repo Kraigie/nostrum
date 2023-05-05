f453fd5d63827583bb7771939c392dccf1964910
Johannes Christ <jc@jchri.st>
2023-05-05 13:00:06 +0200

    Set new version in mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Nostrum.Mixfile do
   def project do
     [
       app: :nostrum,
-      version: "0.6.1",
+      version: "0.7.0",
       elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       build_embedded: Mix.env() == :prod,
