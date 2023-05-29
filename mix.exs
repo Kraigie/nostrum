7a9c8585d3204737259d976c1dc750dcae09445a
Johannes Christ <jc@jchri.st>
2023-05-29 19:19:11 +0200

    Prepare 0.8.0-beta1

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Nostrum.Mixfile do
   def project do
     [
       app: :nostrum,
-      version: "0.8.0-alpha1",
+      version: "0.8.0-beta1",
       elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       build_embedded: Mix.env() == :prod,
