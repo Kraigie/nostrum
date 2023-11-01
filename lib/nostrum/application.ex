3f7c34b1174c6a11f588ade932a82def63a53e89
Brandt Hill <brandtlarsonhill@gmail.com>
2023-11-01 13:18:40 -0500

    Add a check for OTP 25.1 to clarify runtime errors due to :pg.monitor/2

--- lib/nostrum/application.ex
+++ lib/nostrum/application.ex
@@ -22,6 +22,7 @@ defmodule Nostrum.Application do
   def start(_type, _args) do
     Token.check_token!()
     check_executables()
+    check_otp_version()
     Logger.add_translator({Nostrum.StateMachineTranslator, :translate})
 
     children = [
@@ -46,25 +47,36 @@ defmodule Nostrum.Application do
 
     cond do
       is_binary(ff) and is_nil(System.find_executable(ff)) ->
-        Logger.warn("""
+        Logger.warning("""
         #{ff} was not found in your path. By default, Nostrum requires ffmpeg to use voice.
         If you don't intend to use voice with ffmpeg, configure :nostrum, :ffmpeg to nil to suppress.
         """)
 
       is_binary(yt) and is_nil(System.find_executable(yt)) ->
-        Logger.warn("""
+        Logger.warning("""
         #{yt} was not found in your path. Nostrum supports youtube-dl for voice.
         If you don't require youtube-dl support, configure :nostrum, :youtubedl to nil to suppress.
         """)
 
       is_binary(sl) and is_nil(System.find_executable(sl)) ->
-        Logger.warn("""
+        Logger.warning("""
         #{sl} was not found in your path. Nostrum supports streamlink for voice.
         If you don't require streamlink support, configure :nostrum, :streamlink to nil to suppress.
         """)
 
       true ->
         :ok
+    end
+  end
+
+  defp check_otp_version() do
+    _module_info = :pg.module_info()
+
+    unless function_exported?(:pg, :monitor, 2) do
+      Logger.critical("""
+      Your Erlang/OTP version needs to be 25.1 or newer to use Nostrum 0.9 and newer.
+      Current major version: #{System.otp_release()}
+      """)
     end
   end
 end
