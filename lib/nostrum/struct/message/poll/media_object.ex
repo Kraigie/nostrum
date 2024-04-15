109b0d47b49bbab858cc4c9080de079891389f0b
Joe Banks <joe@jb3.dev>
2024-04-15 19:53:30 +0100

    Documentation and simplifications in MediaObject
    Co-authored-by: jchristgit <jc@jchri.st>

--- lib/nostrum/struct/message/poll/media_object.ex
+++ lib/nostrum/struct/message/poll/media_object.ex
@@ -17,10 +17,10 @@ defmodule Nostrum.Struct.Message.Poll.MediaObject do
   @type text :: String.t() | nil
 
   @typedoc """
-  A partial emoji (only supported for answers)
+  A partial emoji (only supported for answers).
 
-  For a custom emoji, only the ID needs to be sent, for a default emoji, only the
-  name field needs to be sent (with the Unicode emoji).
+  For a custom emoji, only the `id` field needs to be sent, for a default emoji, only the
+  `name` field needs to be sent (with the Unicode emoji).
   """
   @type emoji :: %{id: integer | nil, name: String.t() | nil}
 
@@ -31,9 +31,7 @@ defmodule Nostrum.Struct.Message.Poll.MediaObject do
 
   @doc false
   def to_struct(map) do
-    new =
-      map
-      |> Map.new(fn {k, v} -> {Util.maybe_to_atom(k), v} end)
+    new = Map.new(map, fn {k, v} -> {Util.maybe_to_atom(k), v} end)
 
     struct(__MODULE__, new)
   end
