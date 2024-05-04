2465d6fb5f868cd0bf20859b5498772921f38901
Joe Banks <joe@jb3.dev>
2024-05-04 16:51:27 +0100

    Update appup in preparation for 0.9.1

--- appup.ex
+++ appup.ex
@@ -1,9 +1,10 @@
 # See the appup cookbook for instructions:
 # https://www.erlang.org/doc/design_principles/appup_cookbook.html
 {
-  ~c"0.9.0",
+  ~c"0.9.1",
   [
     # Upgrade instructions
+    {~c"0.9.0", []},
     {~c"0.9.0-rc1", []},
     {~c"0.9.0-alpha3", []},
     {~c"0.9.0-alpha2", []},
