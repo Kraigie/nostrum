ea8e90af79b71b45ffa28ac531b05e762c341127
MiniAppleTheApple <Chenxiuhua19780828@gmail.com>
2024-07-27 05:31:28 -0300

    Abstract on new supported components

---
+++ lib/nostrum/struct/component/channel_select.ex
@@ -0,0 +1,43 @@
+defmodule Nostrum.Struct.Component.ChannelSelect do
+  alias Nostrum.Constants.ComponentType
+
+  @defaults %{
+    custom_id: nil,
+    min_values: 1,
+    disabled: false,
+    max_values: 1,
+    channel_types: [],
+    type: ComponentType.channel_select()
+  }
+
+  use Nostrum.Struct.Component
+
+  @type t :: %Component{
+    custom_id: Component.custom_id(),
+    disabled: Component.disabled(),
+    min_values: Component.min_values(),
+    max_values: Component.max_values(),
+    channel_types: Component.channel_types(),
+    type: Component.type()
+  }
+
+  @type opt ::
+          {:custom_id, Component.custom_id()}
+          | {:min_values, Component.min_values()}
+          | {:max_values, Component.max_values()}
+          | {:disabled, Component.disabled()}
+          | {:channel_types, Component.channel_types()}
+
+  @types opts :: [opt]
+
+  def channel_select(custom_id, opts \\ []) when is_binary(custom_id) do
+    [
+      {:custom_id, custom_id},
+      {:disabled, opts[:disabled]},
+      {:min_values, opts[:min_values]},
+      {:max_values, opts[:max_values]}
+      {:channel_types, opts[:channel_types]}
+    ]
+    |> new()
+  end
+end
---
+++ lib/nostrum/struct/component/mentionable_select.ex
@@ -0,0 +1,37 @@
+defmodule Nostrum.Struct.Component.MentionableSelect do
+  alias Nostrum.Struct.Component.SelectMenu
+  alias Nostrum.Constants.ComponentType
+
+  @defaults %{
+    custom_id: nil,
+    placeholder: "",
+    min_values: 1,
+    disabled: false,
+    max_values: 1,
+    type: ComponentType.mentionable_select()
+  }
+
+  use Nostrum.Struct.Component
+
+  @type t :: SelectMenu.t()
+ 
+ @type opt ::
+          {:custom_id, Component.custom_id()}
+          | {:placeholder, Component.placeholder()}
+          | {:min_values, Component.min_values()}
+          | {:max_values, Component.max_values()}
+          | {:disabled, Component.disabled()}
+
+  @type opts :: [opt]
+
+  def mentionable_select(custom_id, opts \\ []) when is_binary(custom_id) do
+    [
+      {:custom_id, custom_id},
+      {:disabled, opts[:disabled]},
+      {:placeholder, opts[:placeholder]},
+      {:min_values, opts[:min_values]},
+      {:max_values, opts[:max_values]}
+    ]
+    |> new()
+  end
+end
