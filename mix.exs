6e1ebe5e46947f630af7a230ca29e92959e45639
Johannes Christ <jc@jchri.st>
2025-02-07 20:13:08 +0100

    Update development dependencies

--- mix.exs
+++ mix.exs
@@ -149,20 +149,20 @@ defmodule Nostrum.Mixfile do
       {:mime, "~> 1.6 or ~> 2.0"},
       {:ezstd, "~> 1.1", optional: true},
       {:castle, "~> 0.3.0", runtime: false},
-      {:ex_doc, "~> 0.32", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.37", only: :dev, runtime: false},
       {:credo, "~> 1.7.7", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.1", only: [:dev], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev], runtime: false},
       {:benchee, "~> 1.1", only: :dev, runtime: false},
-      {:recon, "~> 2.3", only: :dev, optional: true}
+      {:recon, "~> 2.5", only: :dev, optional: true}
     ]
   end
 
   def dialyzer do
     [
-      plt_add_deps: :transitive,
+      plt_add_deps: :app_tree,
       plt_add_apps: [:mix, :mnesia],
-      # "-Wmissing_return"]
-      flags: ["-Wextra_return", "-Werror_handling"]
+      # "-Wmissing_return", "-Wno_return", "-Wunmatched_returns", "-Wunderspecs", "-Woverspecs", "-Wunknown"
+      flags: [:extra_return, :error_handling]
     ]
   end
 end
