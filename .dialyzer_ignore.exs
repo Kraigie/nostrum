a673f149c519301b8567ca2c1d347cf426e098a4
Johannes Christ <jc@jchri.st>
2025-02-07 20:15:05 +0100

    Add ignore file for dialyzer

---
+++ .dialyzer_ignore.exs
@@ -0,0 +1,7 @@
+[
+  {"lib/nostrum/shard/session/compression/zstd.ex", "Function :ezstd.create_decompression_context/1 does not exist."},
+  {"lib/nostrum/shard/session/compression/zstd.ex", "Function :ezstd.decompress_streaming/2 does not exist."},
+  {"lib/nostrum/api/interaction.ex", "The contract for original_response/1 is overloaded."},
+  {"lib/nostrum/struct/message/poll.ex", "The contract for create_poll/2 is overloaded."},
+  {"lib/nostrum/struct/message/poll.ex", "The contract for put_answer/3 is overloaded."},
+]
