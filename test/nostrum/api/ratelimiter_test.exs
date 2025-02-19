f12612600a85b9d09a45d38f3bb157c7b2570a09
Johannes Christ <jc@jchri.st>
2025-02-19 20:46:56 +0100

    Add tests for expiring ratelimits

--- test/nostrum/api/ratelimiter_test.exs
+++ test/nostrum/api/ratelimiter_test.exs
@@ -28,13 +28,13 @@ defmodule Nostrum.Api.RatelimiterTest do
     [ratelimiter: ratelimiter, server: server]
   end
 
-  defp build_request(function, params \\ []) do
+  defp build_request(function, query_params \\ []) do
     %{
       method: :get,
-      route: "/nostrum_test_api_server:#{function}?#{URI.encode_query(params)}",
+      route: "/nostrum_test_api_server:#{function}",
       body: "",
       headers: [{"X-Craigie-Cat", "Hungry"}],
-      params: []
+      params: query_params
     }
   end
 
@@ -51,10 +51,63 @@ defmodule Nostrum.Api.RatelimiterTest do
     end
   end
 
+  describe "accounted requests" do
+    setup do
+      server =
+        start_supervised!(
+          {:nostrum_test_api_server,
+           %{"accounted" => %{remaining: 2, reset_after: 500, timer: nil, total: 2}}}
+        )
+
+      [accountant: server]
+    end
+
+    # These would cause the GenServer that checks the ratelimits to crash
+    test "do not run into the request limit", %{ratelimiter: ratelimiter} do
+      request = build_request("accounted")
+
+      for _ <- 1..3 do
+        reply = Ratelimiter.queue(ratelimiter, request, :timer.seconds(3))
+        assert {:ok, body} = reply
+        assert %{"request" => "received"} = :json.decode(body)
+      end
+    end
+  end
+
+  describe "accounted requests that start out with a 429" do
+    setup do
+      bucket_name = "thoughtcrime"
+      bucket = %{remaining: 0, reset_after: 500, timer: nil, total: 2}
+      buckets = %{bucket_name => bucket}
+      server = start_supervised!({:nostrum_test_api_server, buckets})
+
+      # The server normally starts this on its own when remaining drops to 0.
+      # But because we start out with 0, we gotta start the reset manually.
+      {:ok, _timer} =
+        :timer.apply_after(bucket.reset_after, :nostrum_test_api_server, :reset_bucket, [
+          bucket_name
+        ])
+
+      [accountant: server, bucket_name: bucket_name]
+    end
+
+    # These would cause the GenServer that checks the ratelimits to crash
+    test "wait their turn", %{bucket_name: bucket_name, ratelimiter: ratelimiter} do
+      request = build_request("accounted", bucket_name: bucket_name)
+
+      for _ <- 1..3 do
+        reply = Ratelimiter.queue(ratelimiter, request, :timer.seconds(3))
+        assert {:ok, body} = reply
+        assert %{"request" => "received"} = :json.decode(body)
+      end
+    end
+  end
+
   describe "user limit" do
     test "ceases to send requests", %{ratelimiter: ratelimiter} do
       request = build_request("user_limit")
       reply = Ratelimiter.queue(ratelimiter, request)
+      # TODO: needs monotime measurement to check if time to next request > 200ms
       assert {:ok, body} = reply
       assert %{"request" => "received"} = :json.decode(body)
     end
@@ -64,6 +117,7 @@ defmodule Nostrum.Api.RatelimiterTest do
     test "ceases to send requests", %{ratelimiter: ratelimiter} do
       request = build_request("global_limit")
       reply = Ratelimiter.queue(ratelimiter, request)
+      # TODO: needs monotime measurement to check if time to next request > 200ms
       assert {:ok, body} = reply
       assert %{"request" => "received"} = :json.decode(body)
     end
@@ -71,6 +125,55 @@ defmodule Nostrum.Api.RatelimiterTest do
 end
 
 defmodule :nostrum_test_api_server do
+  use GenServer
+
+  ## normal exports
+
+  def remaining_calls(bucket) do
+    GenServer.call(__MODULE__, {:check_limits, bucket})
+  end
+
+  def reset_bucket(bucket) do
+    :ok = GenServer.call(__MODULE__, {:reset_bucket, bucket})
+  end
+
+  ## mod_esi API
+
+  defp decode_opts([]), do: %{}
+  defp decode_opts(query), do: URI.decode_query(to_string(query))
+
+  def accounted(session, _env, input) do
+    opts = decode_opts(input)
+    remaining = remaining_calls(Map.get(opts, "bucket_name", "accounted"))
+
+    response =
+      if remaining < 0 do
+        """
+        status: 429 Too Many Requests\r
+        Content-Type: application/json\r
+        x-ratelimit-remaining: 0\r
+        x-ratelimit-reset-after: 0.5\r
+        \r
+        {"YOU HAVE BEEN BANNED": "FOREVER!"}
+        """
+      else
+        """
+        status: 200 OK\r
+        Content-Type: application/json\r
+        x-ratelimit-remaining: #{remaining}\r
+        x-ratelimit-reset-after: 0.5\r
+        \r
+        {"request": "received"}
+        """
+      end
+
+    :ok =
+      :mod_esi.deliver(
+        session,
+        to_charlist(response)
+      )
+  end
+
   def endpoint(session, _env, _input) do
     :ok =
       :mod_esi.deliver(
@@ -133,5 +236,60 @@ defmodule :nostrum_test_api_server do
         {"napped": "a bit"}
         """
       )
+  end
+
+  ## GenServer API
+
+  def child_spec(buckets) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, [buckets]},
+      type: :worker,
+      restart: :permanent,
+      shutdown: 500
+    }
+  end
+
+  def start_link(buckets) do
+    GenServer.start_link(__MODULE__, buckets, name: __MODULE__)
+  end
+
+  @impl true
+  def init(buckets) do
+    # Needed for cleanup.
+    Process.flag(:trap_exit, true)
+    {:ok, buckets}
+  end
+
+  @impl true
+  def handle_call({:check_limits, bucket}, _from, buckets) do
+    case Map.get(buckets, bucket) do
+      %{remaining: count, reset_after: interval, timer: nil} = match ->
+        {:ok, timer} = :timer.apply_after(interval, __MODULE__, :reset_bucket, [bucket])
+        remaining = count - 1
+        state = Map.put(buckets, bucket, %{match | remaining: remaining, timer: timer})
+        {:reply, remaining, state}
+
+      %{remaining: count, reset_after: _interval, timer: timer} = match ->
+        remaining = count - 1
+        state = Map.put(buckets, bucket, %{match | remaining: remaining, timer: timer})
+        {:reply, remaining, state}
+
+      nil ->
+        {:reply, nil, buckets}
+    end
+  end
+
+  def handle_call({:reset_bucket, bucket}, _from, buckets) do
+    match = Map.fetch!(buckets, bucket)
+    buckets = Map.put(buckets, bucket, %{match | remaining: match.total, timer: nil})
+    {:reply, :ok, buckets}
+  end
+
+  @impl true
+  def terminate(_reason, buckets) do
+    buckets
+    |> Stream.filter(fn {_key, %{timer: timer}} -> not is_nil(timer) end)
+    |> Enum.each(fn {_key, %{timer: timer}} -> {:ok, :cancel} = :timer.cancel(timer) end)
   end
 end
