acbe7439659f4c01ad4ae7f6ff15a0b6b80c4d2b
Johannes Christ <jc@jchri.st>
2025-03-02 20:58:16 +0100

    Keep outstanding requests on connection crash
    The ratelimiter would previously not take `outstanding` requests with it
    when it would transition to `:disconnected` state, resulting in pending
    requests being silently dropped.
    
    Fixes #669.

--- lib/nostrum/api/ratelimiter.ex
+++ lib/nostrum/api/ratelimiter.ex
@@ -904,7 +904,8 @@ defmodule Nostrum.Api.Ratelimiter do
 
   def connected(:info, {:gun_down, conn, _, reason, killed_streams}, %{
         running: running,
-        wrapped_token: wrapped_token
+        wrapped_token: wrapped_token,
+        outstanding: outstanding
       }) do
     # Even with `retry: 0`, gun seems to try and reconnect, potentially because
     # of WebSocket. Force the connection to die.
@@ -926,7 +927,8 @@ defmodule Nostrum.Api.Ratelimiter do
         {:reply, client, {:error, {:connection_died, reason}}}
       end)
 
-    {:next_state, :disconnected, empty_state(wrapped_token.()), replies}
+    new_data = %{empty_state(wrapped_token.()) | outstanding: outstanding}
+    {:next_state, :disconnected, new_data, replies}
   end
 
   def global_limit(:state_timeout, next, data) do
