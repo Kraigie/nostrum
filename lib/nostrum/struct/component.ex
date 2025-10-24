6cc2c1b5b3c738f6a8bf183b417982343745b59a
SachsKaylee <hi@kaylee-sachs.dev>
2025-10-24 01:22:38 +0000

    Added documentation for select menus

--- lib/nostrum/struct/component.ex
+++ lib/nostrum/struct/component.ex
@@ -74,7 +74,7 @@ defmodule Nostrum.Struct.Component do
   ![Discord Selects](./assets/select_menu.png)
 
   Select menus support single-select and multi-select behavior, meaning you can prompt a user to choose just one item from a list, or multiple. When a user finishes making their choice by clicking out of the dropdown or closing the half-sheet, your app will receive an interaction.
-  - Select menus **must** be sent inside an Action Row
+  - Select menus **must** be sent inside an Action Row or Label
   - An Action Row can contain **only one** select menu
   - An Action Row containing a select menu **cannot** also contain buttons
 
