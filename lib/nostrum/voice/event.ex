96122971776d4a9d1995571b6d38327f0d2d70f2
Brandt Hill <brandtlarsonhill@gmail.com>
2026-03-03 18:08:44 -0600

    Add handlers for dave session lifecycle events

--- lib/nostrum/voice/event.ex
+++ lib/nostrum/voice/event.ex
@@ -1,7 +1,7 @@
 defmodule Nostrum.Voice.Event do
   @moduledoc false
 
-  alias Nostrum.Cache.UserCache
+  alias Nostrum.Cache.Me
   alias Nostrum.Constants
   alias Nostrum.Struct.VoiceWSState
   alias Nostrum.Voice
@@ -18,36 +18,36 @@ defmodule Nostrum.Voice.Event do
 
     payload["op"]
     |> Constants.atom_from_voice_opcode()
-    |> handle_event(payload, state)
+    |> handle_event(payload["d"], state)
   end
 
   defp update_sequence(state, %{"seq" => seq} = _payload), do: %{state | seq: seq}
 
   defp update_sequence(state, _payload), do: state
 
-  defp handle_event(:ready, payload, state) do
+  defp handle_event(:ready, data, state) do
     Logger.debug("VOICE READY")
 
-    mode = Crypto.encryption_mode(state.bot_options, payload["d"]["modes"])
+    mode = Crypto.encryption_mode(state.bot_options, data["modes"])
 
     voice =
       Voice.update_voice(state.voice_pid, state.guild_id,
-        ssrc: payload["d"]["ssrc"],
-        ip: payload["d"]["ip"],
-        port: payload["d"]["port"],
+        ssrc: data["ssrc"],
+        ip: data["ip"],
+        port: data["port"],
         encryption_mode: mode,
         udp_socket: Audio.open_udp()
       )
 
     {my_ip, my_port} = Audio.discover_ip(voice.udp_socket, voice.ip, voice.port, voice.ssrc)
 
-    {%{state | encryption_mode: mode}, Payload.select_protocol_payload(my_ip, my_port, mode)}
+    {%{state | encryption_mode: mode}, Payload.select_protocol(my_ip, my_port, mode)}
   end
 
-  defp handle_event(:session_description, payload, state) do
+  defp handle_event(:session_description, data, state) do
     Logger.debug("VOICE SESSION DESCRIPTION")
 
-    secret_key = payload["d"]["secret_key"] |> :erlang.list_to_binary()
+    secret_key = data["secret_key"] |> :erlang.list_to_binary()
 
     Voice.update_voice_async(state.voice_pid, state.guild_id,
       secret_key: secret_key,
@@ -57,7 +57,9 @@ defmodule Nostrum.Voice.Event do
 
     Session.on_voice_ready(state.conn_pid)
 
-    %{state | secret_key: secret_key}
+    state = %{state | secret_key: secret_key}
+
+    init_dave(data["dave_protocol_version"] || 0, state)
   end
 
   defp handle_event(:heartbeat_ack, _payload, state) do
@@ -70,57 +72,182 @@ defmodule Nostrum.Voice.Event do
     state
   end
 
-  defp handle_event(:hello, payload, state) do
-    state = %{state | heartbeat_interval: payload["d"]["heartbeat_interval"]}
+  defp handle_event(:hello, data, state) do
+    state = %{state | heartbeat_interval: data["heartbeat_interval"]}
 
     GenServer.cast(state.conn_pid, :heartbeat)
 
     if state.identified do
       Logger.info("RESUMING")
-      {state, Payload.resume_payload(state)}
+      {state, Payload.resume(state)}
     else
       Logger.info("IDENTIFYING")
-      {%{state | identified: true}, Payload.identify_payload(state)}
+      {%{state | identified: true}, Payload.identify(state)}
     end
   end
 
-  defp handle_event(:client_connect, payload, state) do
-    Logger.debug(fn ->
-      user_id = payload["d"]["user_id"] |> String.to_integer()
+  defp handle_event(:client_connect, data, %{connected_clients: clients} = state) do
+    user_id = data["user_id"] |> String.to_integer()
 
-      "Voice client connected: #{case UserCache.get(user_id) do
-        {:ok, %{username: username}} -> username
-        _ -> user_id
-      end}"
-    end)
+    Logger.debug("Voice client connected: #{user_id}")
 
-    state
+    %{state | connected_clients: MapSet.put(clients, user_id)}
   end
 
-  defp handle_event(:client_disconnect, payload, state) do
-    Logger.debug(fn ->
-      user_id = payload["d"]["user_id"] |> String.to_integer()
+  defp handle_event(:clients_connect, data, %{connected_clients: clients} = state) do
+    user_ids = data["user_ids"] |> Enum.map(&String.to_integer/1)
 
-      "Voice client disconnected: #{case UserCache.get(user_id) do
-        {:ok, %{username: username}} -> username
-        _ -> user_id
-      end}"
-    end)
+    Logger.debug("Voice clients connected: #{inspect(user_ids)}")
 
-    state
+    %{state | connected_clients: MapSet.union(clients, MapSet.new(user_ids))}
+  end
+
+  defp handle_event(:client_disconnect, data, %{connected_clients: clients} = state) do
+    user_id = data["user_id"] |> String.to_integer()
+
+    Logger.debug("Voice client disconnected: #{user_id}")
+
+    %{state | connected_clients: MapSet.delete(clients, user_id)}
   end
 
   defp handle_event(:codec_info, _payload, state), do: state
 
-  defp handle_event(:speaking, payload, state) do
-    ssrc = payload["d"]["ssrc"]
-    user_id = payload["d"]["user_id"] |> String.to_integer()
+  defp handle_event(:speaking, data, state) do
+    ssrc = data["ssrc"]
+    user_id = data["user_id"] |> String.to_integer()
     ssrc_map = Map.put(state.ssrc_map, ssrc, user_id)
     %{state | ssrc_map: ssrc_map}
   end
 
-  defp handle_event(event, _payload, state) do
-    Logger.debug("UNHANDLED VOICE GATEWAY EVENT #{event}")
+  ## DAVE specific events
+
+  defp handle_event(:dave_prepare_transition, data, state) do
+    protocol_version = data["protocol_version"]
+    transition_id = data["transition_id"]
+
+    state = %{
+      state
+      | pending_transitions: Map.put(state.pending_transitions, transition_id, protocol_version)
+    }
+
+    cond do
+      transition_id == 0 ->
+        execute_transition(transition_id, state)
+
+      protocol_version == 0 ->
+        Dave.set_passthrough_mode(state.dave_session, true)
+
+        {state, Payload.dave_transition_ready(transition_id)}
+
+      true ->
+        state
+    end
+  end
+
+  defp handle_event(:dave_execute_transition, data, state) do
+    execute_transition(data["transition_id"], state)
+  end
+
+  defp handle_event(:dave_prepare_epoch, data, state) do
+    if data["epoch"] == 1,
+      do: init_dave(data["protocol_version"], state),
+      else: state
+  end
+
+  defp handle_event(:dave_mls_external_sender, data, state) do
+    with session when is_reference(session) <- state.dave_session do
+      Dave.set_external_sender(session, data)
+    end
+
     state
+  end
+
+  defp handle_event(:dave_mls_proposals, data, state) do
+    with <<type, proposals::binary>> when type in 0..1 <- data,
+         session when is_reference(session) <- state.dave_session,
+         operation_type <- (type == 0 && :append) || :revoke,
+         %MapSet{} = user_ids <- state.connected_clients,
+         {commit, welcome} when is_binary(commit) <-
+           Dave.process_proposals(session, operation_type, proposals, MapSet.to_list(user_ids)) do
+      {state, Payload.dave_mls_commit_welcome(commit, welcome)}
+    else
+      _ -> state
+    end
+  end
+
+  defp handle_event(
+         :dave_mls_announce_commit_transition,
+         <<transition_id::big-16, commit::binary>>,
+         state
+       ) do
+    with session when is_reference(session) <- state.dave_session,
+         :ok <- Dave.process_commit(session, commit),
+         true <- transition_id > 0 do
+      pending_transitions =
+        Map.put(state.pending_transitions, transition_id, Dave.protocol_version(session))
+
+      {%{state | pending_transitions: pending_transitions},
+       Payload.dave_transition_ready(transition_id)}
+    else
+      :error -> {state, Payload.dave_mls_invalid_commit_welcome(transition_id)}
+      _ -> state
+    end
+  end
+
+  defp handle_event(:dave_mls_welcome, <<transition_id::big-16, welcome::binary>>, state) do
+    with session when is_reference(session) <- state.dave_session,
+         :ok <- Dave.process_welcome(session, welcome),
+         true <- transition_id > 0 do
+      pending_transitions =
+        Map.put(state.pending_transitions, transition_id, Dave.protocol_version(session))
+
+      {%{state | pending_transitions: pending_transitions},
+       Payload.dave_transition_ready(transition_id)}
+    else
+      :error -> {state, Payload.dave_mls_invalid_commit_welcome(transition_id)}
+      _ -> state
+    end
+  end
+
+  defp handle_event(event, data, state) do
+    Logger.debug("UNHANDLED VOICE GATEWAY EVENT #{event}, #{inspect(data)}")
+    state
+  end
+
+  defp execute_transition(
+         transition_id,
+         %{dave_session: session, pending_transitions: pending_transitions} = state
+       )
+       when is_reference(session) and is_map_key(pending_transitions, transition_id) do
+    {protocol_version, pending_transitions} = Map.pop(pending_transitions, transition_id)
+
+    if protocol_version == 0, do: Dave.reset(session)
+
+    if transition_id > 0 and protocol_version > 0, do: Dave.set_passthrough_mode(session, true)
+
+    %{state | pending_transitions: pending_transitions}
+  end
+
+  defp execute_transition(_transition_id, state), do: state
+
+  defp init_dave(0 = _protocol_version, %{dave_session: nil} = state), do: state
+
+  defp init_dave(0 = _protocol_version, %{dave_session: session} = state) do
+    Dave.reset(session)
+    Dave.set_passthrough_mode(session, true)
+    state
+  end
+
+  defp init_dave(protocol_version, %{dave_session: nil} = state) do
+    session = Dave.new_session(protocol_version, Me.get().id, state.channel_id)
+    key = Dave.get_serialized_key_package(session)
+    Voice.update_voice_async(state.voice_pid, state.guild_id, dave_session: session)
+    {%{state | dave_session: session}, Payload.dave_mls_key_package(key)}
+  end
+
+  defp init_dave(protocol_version, %{dave_session: session} = state) do
+    Dave.reinit(session, protocol_version, Me.get().id, state.channel_id)
+    key = Dave.get_serialized_key_package(session)
+    {state, Payload.dave_mls_key_package(key)}
   end
 end
--- lib/nostrum/voice/session.ex
+++ lib/nostrum/voice/session.ex
@@ -72,6 +72,8 @@ defmodule Nostrum.Voice.Session do
       guild_id: voice.guild_id,
       channel_id: voice.channel_id,
       ssrc_map: Map.new(),
+      pending_transitions: Map.new(),
+      connected_clients: MapSet.new(),
       session: voice.session,
       token: voice.token,
       gateway: voice.gateway,
@@ -103,13 +105,12 @@ defmodule Nostrum.Voice.Session do
     GenServer.cast(pid, :voice_ready)
   end
 
-  def handle_info({:gun_ws, _worker, stream, {:text, frame}}, state) do
-    from_handle =
-      frame
-      |> Jason.decode!()
-      |> Event.handle(state)
+  defp handle_gateway_event(payload, state, stream) do
+    case Event.handle(payload, state) do
+      {new_state, <<op_code, _rest::binary>> = reply} when op_code in 25..30 ->
+        :ok = :gun.ws_send(state.conn, stream, {:binary, reply})
+        {:noreply, new_state}
 
-    case from_handle do
       {new_state, reply} ->
         :ok = :gun.ws_send(state.conn, stream, {:text, reply})
         {:noreply, new_state}
@@ -117,6 +118,17 @@ defmodule Nostrum.Voice.Session do
       new_state ->
         {:noreply, new_state}
     end
+  end
+
+  def handle_info({:gun_ws, _worker, stream, {:binary, <<seq::big-16, op::8, d::binary>>}}, state) do
+    payload = %{"seq" => seq, "op" => op, "d" => d}
+    handle_gateway_event(payload, state, stream)
+  end
+
+  def handle_info({:gun_ws, _worker, stream, {:text, frame}}, state) do
+    frame
+    |> Jason.decode!()
+    |> handle_gateway_event(state, stream)
   end
 
   def handle_info({:gun_ws, _conn, _stream, :close}, state) do
@@ -193,7 +205,7 @@ defmodule Nostrum.Voice.Session do
         :heartbeat
       ])
 
-    :ok = :gun.ws_send(state.conn, state.stream, {:text, Payload.heartbeat_payload(state)})
+    :ok = :gun.ws_send(state.conn, state.stream, {:text, Payload.heartbeat(state)})
 
     {:noreply,
      %{state | heartbeat_ref: ref, heartbeat_ack: false, last_heartbeat_send: DateTime.utc_now()}}
@@ -201,7 +213,7 @@ defmodule Nostrum.Voice.Session do
 
   def handle_cast(:voice_ready, state) do
     voice = Voice.get_voice(state.voice_pid, state.guild_id)
-    voice_ready = Payload.voice_ready_payload(voice)
+    voice_ready = Payload.voice_ready(voice)
 
     Dispatch.handle(voice_ready, state)
 
@@ -210,8 +222,8 @@ defmodule Nostrum.Voice.Session do
 
   def handle_cast({:speaking, speaking, timed_out}, state) do
     voice = Voice.update_voice(state.voice_pid, state.guild_id, speaking: speaking)
-    speaking_update = Payload.speaking_update_payload(voice, timed_out)
-    payload = Payload.speaking_payload(voice)
+    speaking_update = Payload.voice_speaking_update(voice, timed_out)
+    payload = Payload.speaking(voice)
 
     Dispatch.handle(speaking_update, state)
 
